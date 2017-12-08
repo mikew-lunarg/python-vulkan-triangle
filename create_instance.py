fbb44fa06d762e57cd105a876a79abea2dfd1126
Mike Weiblen <mikew@lunarg.com>
2017-12-08 15:46:14 -0700

    tweaks

--- create_instance.py
+++ create_instance.py
@@ -6,23 +6,17 @@ from ctypes import c_void_p, c_float, c_uint8, c_uint, c_uint64, c_int, c_size_t
 import platform
 
 # platform-specific library initialization
-system_name = platform.system()
-if system_name == 'Windows':
+platform_name = platform.system()
+if platform_name == 'Windows':
     from ctypes import WINFUNCTYPE, windll
     FUNCTYPE = WINFUNCTYPE
     vk = windll.LoadLibrary('vulkan-1')
-elif system_name == 'Linux':
+elif platform_name == 'Linux':
     from ctypes import CFUNCTYPE, cdll
     FUNCTYPE = CFUNCTYPE
     vk = cdll.LoadLibrary('libvulkan.so.1')
-
-def MAKE_VERSION(major, minor, patch):
-    return (major<<22) | (minor<<12) | patch
-API_VERSION_1_0 = MAKE_VERSION(1,0,0)
-API_VERSION_1_1 = MAKE_VERSION(1,1,0)
-
-def define_structure(name, *args):
-    return type(name, (Structure,), {'_fields_': args})
+else:
+    raise RuntimeError('Unsupported platform {}'.format(platform_name))
 
 # HANDLES
 Instance = c_size_t
@@ -32,6 +26,11 @@ InstanceCreateFlags = c_uint
 
 # Enumeration definition ####################################################
 
+def MAKE_VERSION(major, minor, patch):
+    return (major<<22) | (minor<<12) | patch
+API_VERSION_1_0 = MAKE_VERSION(1,0,0)
+API_VERSION_1_1 = MAKE_VERSION(1,1,0)
+
 StructureType = c_uint
 STRUCTURE_TYPE_APPLICATION_INFO = 0
 STRUCTURE_TYPE_INSTANCE_CREATE_INFO = 1
@@ -40,6 +39,9 @@ Result = c_uint
 SUCCESS = 0
 
 # Structure definition ######################################################
+
+def define_structure(name, *args):
+    return type(name, (Structure,), {'_fields_': args})
 
 ApplicationInfo = define_structure('ApplicationInfo',
     ('s_type', StructureType),
@@ -62,20 +64,11 @@ InstanceCreateInfo = define_structure('InstanceCreateInfo',
     ('enabled_extension_names', POINTER(c_char_p)),
 )
 
-# FUNCTIONS
+# Function definition #######################################################
 
 GetInstanceProcAddr = vk.vkGetInstanceProcAddr
 GetInstanceProcAddr.restype = FUNCTYPE( None, )
 GetInstanceProcAddr.argtypes = (Instance, c_char_p, )
-print("hello1")
-
-LoaderFunctions = (
-    (b'vkCreateInstance', Result, POINTER(InstanceCreateInfo), c_void_p, POINTER(Instance), ),
-)
-
-InstanceFunctions = (
-    (b'vkDestroyInstance', None, Instance, c_void_p ),
-)
 
 def load_functions(vk_object, functions_list, load_func):
     functions = []
@@ -94,6 +87,15 @@ def load_functions(vk_object, functions_list, load_func):
         functions.append((py_name, fn))
     return functions
 
+LoaderFunctions = (
+    (b'vkCreateInstance', Result, POINTER(InstanceCreateInfo), c_void_p, POINTER(Instance), ),
+)
+
+InstanceFunctions = (
+    (b'vkDestroyInstance', None, Instance, c_void_p ),
+)
+
+# Structure instances #######################################################
 
 app_info = ApplicationInfo(
     s_type = STRUCTURE_TYPE_APPLICATION_INFO,
@@ -116,13 +118,13 @@ create_info = InstanceCreateInfo(
     enabled_extension_names = None
 )
 
+# main() ####################################################################
+
 instance = Instance(0)
 
-print("hello3")
 f=locals()
 for name, fnptr in load_functions(instance, LoaderFunctions, GetInstanceProcAddr):
     f[name] = fnptr
-print("hello4")
 
 print('CreateInstance');
 result = CreateInstance(byref(create_info), None, byref(instance))
@@ -132,8 +134,8 @@ if result != SUCCESS:
 f = locals()
 for name, fnptr in load_functions(instance, InstanceFunctions, GetInstanceProcAddr):
     f[name] = fnptr
-
 print('SUCCESS {}'.format(instance))
+
 print('DestroyInstance')
 DestroyInstance(instance, None)
 instance = None
